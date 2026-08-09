1401d158811a49e1d4a3585f99feb47dc1d0ea08
Ganji Sai_Kiran <ganjisaikiran04@gmail.com>
2026-08-09 13:41:22 +0530

    Format Terraform files

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "buckeforfractice4477"
-    key            = "dev/terraform.tfstate"
-    region         = "ap-southeast-1"
+    bucket       = "buckeforfractice4477"
+    key          = "dev/terraform.tfstate"
+    region       = "ap-southeast-1"
     encrypt      = true
     use_lockfile = true
-}
+  }
 }
--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@
 data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "main" {
-	cidr_block           = "10.0.0.0/16"
-	enable_dns_support   = true
-	enable_dns_hostnames = true
-	tags = {
-		Name = "main-vpc"
-	}
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main-vpc"
+  }
 }
 # resource "aws_internet_gateway" "igw" {
 # 	vpc_id = aws_vpc.main.id
