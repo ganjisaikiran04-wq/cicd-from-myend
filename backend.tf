8544ca3f2efc93a93a1b04026f72a6e8f84b037b
Ganji Sai_Kiran <ganjisaikiran04@gmail.com>
2026-08-09 13:44:10 +0530

    Format Terraform files

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "buckeforfractice4477"
+    bucket       = "buckeforpractice4477"
     key          = "dev/terraform.tfstate"
     region       = "ap-southeast-1"
     encrypt      = true
